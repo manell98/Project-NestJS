976a91e2b61c58a431d17cf39a88d49670ef7d0f
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2021-05-22 18:54:01 -0300

    Add typeorm e configurando docker-compose

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,11 +1,27 @@
 export interface IEnvironment {
   app: {
     logLevel: string;
+    port: number;
+  };
+  typeOrm: {
+    type: any;
+    host: string;
+    port: number;
+    database: string;
+    entities: any;
   };
 }
 
 export const environment: IEnvironment = {
   app: {
-    logLevel: 'debug',
+    logLevel: process.env.LOG_LEVEL || 'info',
+    port: Number(process.env.BD_PORT) || 3000,
+  },
+  typeOrm: {
+    type: process.env.BD_TYPE || 'mongodb',
+    host: process.env.BD_HOST || 'mongodb',
+    port: Number(process.env.BD_PORT) || 27017,
+    database: process.env.BD_DATABASE || 'nest-js-db',
+    entities: ['src/entity/**/*.ts'],
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { environment } from './config/environment';
+import Database from './config/database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(environment.app.port, async () => {
+    await Database.conectar();
+  });
 }
 bootstrap();
