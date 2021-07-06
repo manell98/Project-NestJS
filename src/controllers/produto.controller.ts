f258606c744349be33a6bfe4304f40abaa3b7867
C1314489 Emanuell Santos de Jesus <c1314489@interno.bb.com.br>
2021-07-05 23:16:30 -0300

    [UPT] - Refactory no nome da classe

--- src/controllers/produto.controller.ts
+++ src/controllers/produto.controller.ts
@@ -1,23 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProdutoService } from '../services/produto.service';
-import { IProduto } from '../models/produto';
+import { Produto } from '../models/produto';
 
 @Controller('produto')
 export class ProdutoController {
   constructor(private readonly produtoService: ProdutoService) {}
 
   @Get()
-  findAll(): Promise<Array<IProduto>> {
+  findAll(): Promise<Array<Produto>> {
     return this.produtoService.findAll();
   }
 
   @Get(':id')
-  findById(@Param() params): Promise<IProduto> {
+  findById(@Param() params): Promise<Produto> {
     return this.produtoService.findById(params.id);
   }
 
   @Post()
-  save(@Body() produto: IProduto): Promise<IProduto> {
+  save(@Body() produto: Produto): Promise<Produto> {
     return this.produtoService.save(produto);
   }
 }
--- src/services/produto.service.ts
+++ src/services/produto.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { logger } from '../utils/logger';
-import { IProduto } from '../models/produto';
+import { Produto } from '../models/produto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class ProdutoService {
   constructor(
-    @InjectModel('Produto') private readonly produtoModel: Model<IProduto>,
+    @InjectModel('Produto') private readonly produtoModel: Model<Produto>,
   ) {}
 
-  async findAll(): Promise<Array<IProduto>> {
+  async findAll(): Promise<Array<Produto>> {
     logger.info('Buscando todos os produtos...');
 
     return this.produtoModel.find();
   }
 
-  async findById(id: string): Promise<IProduto> {
+  async findById(id: string): Promise<Produto> {
     logger.info(`Buscando produto de id: ${id}...`);
 
     return this.produtoModel.findById(id);
   }
 
-  async save(produto: IProduto): Promise<IProduto> {
+  async save(produto: Produto): Promise<Produto> {
     logger.debug('Salvando produto...');
 
     const result = new this.produtoModel(produto);
